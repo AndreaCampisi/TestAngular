6cb2a9836f0ce060ef58ee8b8e565a4c4c470dff
Andrea Campisi <ilcampo94@gmail.com>
2025-02-21 15:55:58 +0100

    emit event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,23 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar !: string;
-  // @Input({ required: true }) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  imagePath = computed(() => {
-    return "assets/users/" + this.avatar();
-  });
-
-  // get imagePath() {
-  //   return "assets/users/" + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-
+    this.select.emit(this.id);
   }
 }
