e444d800317dcf901a9b993f4c64580e7a06160a
Andrea Campisi <ilcampo94@gmail.com>
2025-02-21 19:07:14 +0100

    add a new task form and handle the oncancel action

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,60 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
 
+  @Input({required: true}) userId?: string;
+  @Input({required: true}) name?: string;
+  @Input() isAddingTask?: boolean;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn Angular and become a pro!',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u1',
+      title: 'Master Css',
+      summary: 'Learn Css and become a pro!',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 't3',
+      userId: 'u2',
+      title: 'Master Ts',
+      summary: 'Learn Ts and become a pro!',
+      dueDate: '2021-12-31',
+    },
+  ];
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+    }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(task: { title: string; summary: string; dueDate: string }) {
+   
+    this.isAddingTask = false;
+  }
 }
