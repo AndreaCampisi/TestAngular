99e9175f241659b19709b37c95dbc0f7a0229460
Andrea Campisi <ilcampo94@gmail.com>
2025-02-22 14:45:54 +0100

    start migration from the standalone component to module approach, we can mix them in the beginning

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+//when you don't use standalone component
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
